0d47a44f633327bdcb40b9b4ebc21135aef5a544
Maksim Lialkin <maksimlialkin1986@gmail.com>
2020-07-08 22:37:15 +0300

    start adaptive

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ $(function () {
     nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
     autoplay: true,
     fade: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+      }
+    }, ]
   });
 
   $('.our-trip__slider').slick({
@@ -11,5 +17,28 @@ $(function () {
     nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
     autoplay: true,
     fade: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+      }
+    }, ]
   });
+
+  $(".menu, .our-trip__body").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
 });
